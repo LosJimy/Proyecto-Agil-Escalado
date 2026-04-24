f336d3c45711944ca1e12ddbc5ca497bf16d5e45
Pablo <pablocortes1101@gmail.com>
2026-04-24 02:17:48 -0400

    refactor: guardar llaves PEM en Base64

--- backend/src/shared/utils/jwt.ts
+++ backend/src/shared/utils/jwt.ts
@@ -1,11 +1,16 @@
 import jwt from "jsonwebtoken";
 
-// Delete newlines from env vars (if any)
-const privateKey = (process.env.JWT_PRIVATE_KEY || "").replace(/\\n/g, "\n");
-const publicKey = (process.env.JWT_PUBLIC_KEY || "").replace(/\\n/g, "\n");
+const privateKey = Buffer.from(
+  process.env.JWT_PRIVATE_KEY || "",
+  "base64",
+).toString("utf8");
+const publicKey = Buffer.from(
+  process.env.JWT_PUBLIC_KEY || "",
+  "base64",
+).toString("utf8");
 
 if (!privateKey || !publicKey) {
-  console.warn("⚠️ JWT keys are not set in environment variables.");
+  console.warn("⚠️ JWT keys are not set in environment variables");
 }
 
 export interface JWTPayload {
